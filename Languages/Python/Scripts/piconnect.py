67441478d47e9c8c3eab267221e63cf0778493d3
erik <erikdkosberg@gmail.com>
2022-03-06 00:25:42 -0600

    Remove piconnect

--- Languages/Python/Scripts/piconnect.py
+++
@@ -1,9 +0,0 @@
-import os
-pi = input("Which pi would you like to connect to? ('s' for Server; 'r' for Robot')")
-
-pi_map = {
-	'r': '192.168.86.47',
-	's': '192.168.86.48'
-}
-
-os.system("ssh pi@%s" % pi_map[pi])
