99a8b2c2ff66d625116b99c1b92823667e77a0af
erik <erikdkosberg@gmail.com>
2022-03-05 19:00:02 -0600

    got rid of C

--- Languages/C/Reference Scripts/listing0504.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("%d/%d=%d\n",2,5,2/5);
-    return(0);
-}
--- Languages/C/Reference Scripts/listing1907.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    const char *sample = "From whence cometh my help?\n";
-
-    puts(sample);
-    return(0);
-}
