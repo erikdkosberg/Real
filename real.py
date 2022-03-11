68f694f195c46d7f87675539c193e6d4f5b7c9e0
erik <erikdkosberg@gmail.com>
2022-03-11 00:10:27 -0600

    Modify real.py to OOP

--- real.py
+++ real.py
@@ -1,8 +1,33 @@
 import os
-os.chdir("Server/")
 
-# Previous method - SimpleHttpServer in Python
-# os.system("python3 -m http.server 8000")
+class Server:
+    """"
+    PHP localhost interface with argument parsing
+    done at the command line.
 
-# New method - using php
-os.system("php -S localhost:8000")
+    Our 'Server' will host three main files:
+        1.) index.html
+        2.) script.js
+        3.) style.css
+    
+    """
+
+    # Go to the server directory
+    def __init__(self):
+        os.chdir("Server/")
+
+
+    # Start via system calls to php on the localhost
+    def start_server(self):
+        os.system("php -S localhost:8000")
+        os.system("clear") # php will pause execution
+        return "SERVER DOWN..." 
+
+
+    # Parse out any arguments to modify server configuration
+    def main(self):
+        # TODO - arg parsing before server starts
+        return self.start_server()
+
+
+print(Server().main())
